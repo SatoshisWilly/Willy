fcb33ade4fc41156f8670f2fef0ad867bb623590
Willy <satoshiswilly@gmail.com>
2024-11-26 13:24:44 -0500

    Update post.ts

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -34,7 +34,7 @@ About {{agentName}} (@{{twitterUserName}}):
 
 # Task: Generate a post in the voice and style of {{agentName}}, aka @{{twitterUserName}}
 Write a single sentence post that is {{adjective}} about {{topic}} (without mentioning {{topic}} directly), from the perspective of {{agentName}}. Try to write something totally different than previous posts. Do not add commentary or acknowledge this request, just write the post.
-Your response should not contain any questions. Brief, concise statements only. No emojis. Use \\n\\n (double spaces) between statements.`;
+Your response should not contain any questions. Brief, concise statements only. use emojis. use hashtags #Willy, #WillytoaBilly, #WillytuahBilly, #BeleiveWithUs, #WillysBelievers, #ArmyofBelievers, #NothingPumpsHarder  Use \\n\\n (double spaces) between statements.`;
 
 interface PostSchedule {
     minMinutes: number;
